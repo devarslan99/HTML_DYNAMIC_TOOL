f94794484b49c51594f9f865f7ba43e47bec211b
Muhammad Arslan <122959770+devarslan99@users.noreply.github.com>
2024-10-14 02:52:04 +0500

    change the config file

--- frontend/config.js
+++ frontend/config.js
@@ -1,7 +1,7 @@
 
 
-// const BASE_URL = "https://html-dynamic-tool.vercel.app"
-const BASE_URL = "http://localhost:3000"
+const BASE_URL = "https://html-dynamic-tool.vercel.app"
+// const BASE_URL = "http://localhost:3000"
 
 export{
     BASE_URL
